3395cc4296990d067ab08d4bdde13afa27a31c8a
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2024-12-24 12:45:44 +0100

    aumentato budget

--- Tasker/frontend/src/app/environment/environtments.ts
+++ Tasker/frontend/src/app/environment/environtments.ts
@@ -5,7 +5,7 @@ export const environment = {
 
 export const environment_prod = {
   production: false,
-  BACKEND_URL: 'http://127.0.0.1:3000/api'
+  BACKEND_URL: 'https://tasker.divisionenergia.it/api'
 };
 // FUNC
 // 'http://127.0.0.1:3000/api' per farlo funzionare in locale
